372b8a5dd72677d38cc3579742a7cc3bc29865be
makachanm <hyeonseongyang09@gmail.com>
2025-09-07 15:16:21 +0900

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 ./cutter -i main.cm -w index.html
+
+for cm_file in articles/*.cm; do
+  html_file="${cm_file%.cm}.html"
+  ./cutter -i "$cm_file" -w "$html_file"
+done
+
+echo "Build complete."
